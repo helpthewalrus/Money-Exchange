074108f179d9f53dab7bf57c0dc1cb34e2e7bbbc
helpthewalrus <alinatylets@gmail.com>
2018-09-27 00:38:15 +0300

    task_5_commmit_1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
-// PLEASE DON'T change function name
-module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+
+  module.exports =  function makeExchange(currency) {
+    const result = {};
+    if (currency > 10000) {
+        result.error = 'You are rich, my friend! We don\'t have so much coins for exchange'; 
+    } else if (currency > 0) {
+        const   h1 = currency % 50;
+                q1 = h1 % 25;
+                d1 = q1 % 10;
+                h = Math.floor(currency / 50),
+                q = Math.floor(h1 / 25),
+                d = Math.floor(q1 / 10),
+                n = Math.floor(d1 / 5),
+                p = d1 % 5;
+        if (h) {
+            result["H"] = h;
+        }
+        if (q) {
+            result["Q"] = q;
+        }
+        if (d) {
+            result["D"] = d;
+        }
+        if (n) {
+            result["N"] = n;
+        }
+        if (p) {
+            result["P"] = p;
+        }
+    }
+    return result;
 }
